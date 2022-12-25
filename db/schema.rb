2c3daba43b0456f83ff366f36cd20a1e191f0027
yshmarov <yshmarov@gmail.com>
2022-12-25 10:42:49 +0100

    scaffold games

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_24_215925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.string "title"
+    t.text "description"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
